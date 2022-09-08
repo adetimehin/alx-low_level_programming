ac586ea346e6b615d46eb49226c428fe1746a3df
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-08 07:22:12 -0700

    Hello, C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/* 4-puts.c : 4. Hello, puts */
+#include <stdio.h>
+
+/**
+ * main - prints with puts
+ *
+ * Return - Returns 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle);
+	return(0);
+}
