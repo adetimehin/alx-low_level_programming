bf1107314c6f4e9b3c0785816a850ee0561f8841
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-08 07:59:40 -0700

    Hello C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/* 5-printf.c : hello, printf */
+#include <stdio.h>
+
+/**
+ * main - prints using printf
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	printf(“with proper grammar, but the outcome is a piece of art, \n”);
+	return (0);
+}
