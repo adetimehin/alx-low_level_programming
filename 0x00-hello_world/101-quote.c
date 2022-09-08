3fa3f6102bd08d6a1b15f05b81a29bec8a1ec67b
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-08 14:03:24 -0700

    Hello C extra

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/* 101-quote.c : Quote */
+#include <stdio.h>
+
+/**
+ * main - prints quote to stderr
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
