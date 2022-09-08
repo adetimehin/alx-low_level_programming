aa019bac22c3ff73e3450227608957ab2843e803
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-08 14:51:57 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19
-\n");
-return (1);
+	char text;
+
+	text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%c\n", text);
+	return (1);
 }
