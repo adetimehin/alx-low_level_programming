88e9c09527bfd6e5a4fff68e3eb6b87f1a9a29c2
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-09 16:16:03 -0700

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio>
+
+/**
+ * main - prints two digit number combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	a = b;
+	do {
+		for (a = 48; a < 58; a < b; a++)
+		{
+			putchar(a);
+			putchar(b);
+			if (a == 56 && b == 57)
+				break;
+			putchar(44);
+			putchar(32);
+		}
+	} while (!(a == b));
+	putchar(10);
+	return (0);
+}
