1bf360066c4d2b98d221f7a19f2af7e4da53839b
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-09 14:18:07 -0700

    minus q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets minus q & e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a != 100; a !=  112; a < 123; a++)
+		putchar(a);
+	putchar(10);
+	return (0);
+}
