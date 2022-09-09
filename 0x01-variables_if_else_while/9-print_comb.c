13dbf463cc6b42d22d79e407be1193562c995c5d
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-09 14:08:04 -0700

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int a;
 
-	for (a = 48; a < 57; a++)
+	for (a = 48; a < 58; a++)
 	{
 		putchar(a);
+		if (a == 57)
+			break;
 		putchar(44);
 		putchar(32);
 	}
-	putchar(57);
+	putchar(10);
 	return (0);
 }
