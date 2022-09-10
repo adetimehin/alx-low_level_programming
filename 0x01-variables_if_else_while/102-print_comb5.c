88d767e7b7433aae3febaae1139abc800255e69f
Emmanuel Adetimehin <mailstomande@gmail.com>
2022-09-09 18:41:04 -0700

    double double

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,25 @@ int main(void)
 
 	for (a = 48; a < 58; a++)
 	{
-		putchar(a);
-		putchar(a);
-		putchar(32);
-		putchar(a);
-		putchar(a++);
-		putchar(44);
-		putchar(32);
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 58; c++)
+			{
+				for (d = a + 1; d < 58; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+					if (!((a == 57 && b == 56) && (c == 57 && d == 57)))
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
 	}
 	putchar(10);
 	return (0);
